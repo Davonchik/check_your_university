38109b061233e0123d20bf3bfab51230b456adf3
Давид <david@MacBook-Pro-David.local>
2025-02-04 22:42:57 +0300

    s3 + request tests dao

--- core/tests/test_s3.py
+++ core/tests/test_s3.py
@@ -4,13 +4,12 @@ from src.infrastructure.database.database import Base
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 from src.infrastructure.database.dao.admin_dao import AdminDao
+from src.infrastructure.database.dao.s3_dao import S3Dao
 from src.infrastructure.database.dao.building_dao import BuildingDao
-from src.infrastructure.database.dao.request_dao import RequestDao
 from src.application.domain.user import UserCreate
-from src.application.domain.request import RequestCreate
-from src.infrastructure.database.models.request import Request
-from src.infrastructure.database.models.user import User
 from src.infrastructure.database.models.admin import Admin
+from src.infrastructure.database.models.s3 import S3
+from src.infrastructure.database.models.user import User
 from src.infrastructure.database.models.building import Building
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import create_async_engine
@@ -32,40 +31,14 @@ async def session():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
+
 @pytest.mark.asyncio
-async def test_create_request(session: AsyncSession):
-    dao = RequestDao(session)
+async def test_create_file(session: AsyncSession):
+    dao = S3Dao(session)
     user = User(tg_id="123user")
-    building = Building(name="pokra")
     session.add(user)
-    session.add(building)
     await session.commit()
     await session.refresh(user)
-    await session.refresh(building)
-    request_in = RequestCreate(
-        user_id=user.id,
-        building_name="pokra",
-        category="test",
-        room="test",
-        text="test"
-    )
-    req = await dao.create_request(request_in)
-    assert req.user_id == user.id
 
-# @pytest.mark.asyncio
-# async def test_get_requests(session: AsyncSession):
-#     dao = RequestDao(session)
-#     user = User(tg_id="123user")
-#     session.add(user)
-#     await session.commit()
-#     await session.refresh(user)
-#     request_in = RequestCreate(
-#         user_id=user.id,
-#         building_name="pokra",
-#         category="test",
-#         room="test",
-#         text="test"
-#     )
-#     req = await dao.create_request(request_in)
-#     requests = await dao.get_requests(user.id)
-#     assert requests[0].id == req.id
+    s3 = await dao.create_file(user_id=user.id, file_name="test", file_url="test_url")
+    assert s3.file_name == "test"
